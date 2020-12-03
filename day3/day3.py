6c6c817ba29366d52de47a2bee8b2c06ead55cfd
TientjieIT <tientjieit@gmail.com>
2020-12-03 11:39:59 +0100

    part1 verbeterd part 2 is af

--- day3/day3.py
+++ day3/day3.py
@@ -1,18 +1,22 @@
-def part1(input):
+def part1(input, slope):
     count = 0
     row = 0
     col = 0
 
     # om index error te voorkomen
+    bottom = len(input)-1
     col_max = len(input[0])
 
     # voor ieder rij behalve de laatste vanwege index error
-    for _ in range(len(input)-1):
-        col += 3
-        row += 1
+    while row != bottom:
+        col += slope[0]
+        row += slope[1]
 
         if col >= col_max:
             col -= col_max
+
+        if row > bottom:
+            row = bottom
 
         if input[row][col] == '#':
             count += 1
@@ -20,15 +24,22 @@ def part1(input):
     return count
 
 
-def part2(input, slopex, slopey):
-    pass
+def part2(input, slopes):
+    total = 1
+    for slope in slopes:
+        print(slope)
+        total *= part1(input, slope)
+    return total
+
 
 
 input_file = open('input.txt', "r")  # opens the file in read mode
 input = [list(line) for line in input_file.read().splitlines()]
 input_file.close()
 
-print(part1(input))
-# print(part2(input))
+print(part1(input, (3, 1)))
+slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+print(part2(input, slopes))
+
 
 
